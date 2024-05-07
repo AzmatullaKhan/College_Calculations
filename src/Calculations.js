00009e2ad1ea1f2a521f48ad3f8d689b6a8f80cd
klu_2200031618 <kk258786@gmail.com>
2024-05-07 13:16:41 +0530

    Logic for getting number of containers entered is completed and need to make a change

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,9 +1,38 @@
 import './Calculations.css'
 
 function Calculations() {
+  const generate=()=>{
+    let es=document.getElementById('each-subject');
+    let in1=document.createElement("input");
+    in1.className='subject-input'
+    in1.placeholder='Enter the expected/acheived grade'
+    in1.type='number'
+    let in2=document.createElement("input");
+    in2.className='subject-input'
+    in2.placeholder='Enter Number of Credits'
+    in1.type='number'
+    let br=document.createElement('br');
+    es.appendChild(in1)
+    es.appendChild(in2)
+    es.appendChild(br)
+  }
+  const numberOfSubjects=()=>{
+    let nos=document.getElementById('numberOfSubjects').value;
+    for(let i=0;i<nos;i++){
+      generate();
+    }
+  }
   return (
-    <div className="App">
-      HI
+    <div className="main">
+      <div className='container'>
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+              <p style={{textDecoration:'underline'}}>No.Of Subjects:</p>
+              <input type='number' className='input-numberOfSubjects' placeholder='Enter Number of Subjects' id='numberOfSubjects'/>
+              <button onClick={numberOfSubjects}>Submit</button>
+            </div>
+            <div className='each-subject' id='each-subject'>
+            </div>
+      </div>
     </div>
   );
 }
